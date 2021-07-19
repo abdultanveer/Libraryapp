a7e1e2a42926de1c395a39ec594a24400a73946f
tejasranka <rankatm17.instru@coep.ac.in>
2021-07-19 10:44:29 +0530

    onitemclick for listview

--- app/src/main/java/com/abdul/libraryapp/BookListActivity.kt
+++ app/src/main/java/com/abdul/libraryapp/BookListActivity.kt
@@ -2,10 +2,22 @@ package com.abdul.libraryapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.Toast
 
-class BookListActivity : AppCompatActivity() {
+class BookListActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
+    lateinit var booklistView: ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_list)
+        booklistView = findViewById(R.id.bookslictview)
+        booklistView.setOnItemClickListener(this)
+    }
+
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        var itemClicked = parent?.getItemAtPosition(position).toString()
+        Toast.makeText(this,itemClicked,Toast.LENGTH_SHORT).show()
     }
 }
